30fc40c3165e9d05976429d1df043f73d25c5534
NeonLider <aceraspiredasturchi@gmail.com>
2026-02-10 14:59:06 +0500

    add code

---
+++ script.js
@@ -0,0 +1,15 @@
+const input = document.getElementById('taskInput');
+const taskList = document.getElementById('taskList');
+
+function vazifaQoshish() {
+    const taskText = input.value.trim();
+    if (taskText) return 
+
+
+
+
+
+
+
+
+}
