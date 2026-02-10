9334b1aef0c43135ac806c6f867e1c7e52b359fc
NeonLider <aceraspiredasturchi@gmail.com>
2026-02-10 15:25:54 +0500

    add and delete code

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-const input = document.getElementById('taskInput');
-const taskList = document.getElementById('taskList');
+const input = document.getElementById("task");
+const list = document.getElementById("list");
 
 function vazifaQoshish() {
-    const taskText = input.value.trim();
-    if (taskText) return 
+  const text = input.value.trim();
+  if (text.length === 0) return;
 
+  const li = document.createElement("li");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
 
+  const span = document.createElement("p");
+  span.textContent = text;
 
+  const delBtn = document.createElement("button");
+  delBtn.textContent = "Delete";
+  delBtn.className = "delete";
 
+  checkbox.onchange = () => {
+    span.classList.toggle("done");
+  };
 
+  delBtn.onclick = () => {
+    li.remove();
+  };
 
+  li.append(checkbox, span, delBtn);
+  list.appendChild(li);
 
-
+  input.value = "";
 }
+
+input.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") vazifaQoshish();
+});
